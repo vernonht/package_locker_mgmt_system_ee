6bd6e80dc740e42e5fb95f38dfeb5aa03a1154d4
Jian Hao <vernonht@gmail.com>
2026-05-17 17:51:21 +0800

    ui: limit parcel dimension input to positive interger

--- components/DepositForm.tsx
+++ components/DepositForm.tsx
@@ -19,6 +19,7 @@ function Field({ label, name, value, onChange, type = 'text', placeholder = '' }
         name={name}
         type={type}
         value={value}
+        min={1}
         onChange={e => onChange(e.target.value)}
         placeholder={placeholder}
         className="border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
